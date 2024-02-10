b2f9cffd0a5055f81859a75ca239842ffe943609
joelthenry <joelthenry@gmail.com>
2024-02-10 13:35:00 -0700

    Adding some .css files as well as the pin system

--- app-skeleton/src/App.jsx
+++ app-skeleton/src/App.jsx
@@ -5,15 +5,18 @@ import './App.css'
 import HomePage from './HomePage';
 import UserInfo from './UserInfo';
 import Acommadations from './Acommadations';
-
 import React from 'react';
 import LoginPage from './LoginPage';
+
+import Fr from '../fr.input/fr';
 
 function App() {
   return (
     <div className="App">
       // Landing Page with authorization
       <h1>Welcome to Disembark!</h1>
+
+
       <LoginPage/>
       <h2>_____________________________</h2>
       // Home Page 
--- app-skeleton/src/HomePage.jsx
+++ app-skeleton/src/HomePage.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import homepageImage from './homepage-image.jpg'; // Import the image file
+import Tag from '../Tag.input/Tag';
+import Pin from '../Pin/Pin';
 
 function HomePage() {
   return (
     <div>
       <h2>Welcome to Our Website!</h2>
       <p>This is the homepage of our app.</p>
+      <Pin cat='GlutenFree' />
       <img src={homepageImage} alt="Homepage" />
+      <Tag name="Gluten Free"/>
+      <Tag name="Wheelchair"/>
     </div>
   );
 }
