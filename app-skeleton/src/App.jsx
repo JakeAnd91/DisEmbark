611c128c6edc576732bfdd9f2993ac8f37ee6265
Jake Anderson <jake.anderson0291@gmail.com>
2024-02-10 11:05:32 -0700

    began working on the components, did a garbage job

--- app-skeleton/src/App.jsx
+++ app-skeleton/src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import HomePage from './HomePage';
 import UserInfo from './UserInfo';
 import Acommadations from './Acommadations';
+import MyComponent from './components/button.icon/home.icon.button';
 
 import React from 'react';
 import LoginPage from './LoginPage';
@@ -25,6 +26,7 @@ function App() {
       // Accomadations Page
       <Acommadations />
       <h2>_____________________________ </h2>
+      <MyComponent />
 
 
       
